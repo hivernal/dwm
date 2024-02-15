81081ca140ad08685b609e0ecedca07a73996fbd
hivernal <hivernal@example.com>
2024-02-15 16:45:32 +0700

    add picom to autostart script

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-picom -b
+picom -b --config /dev/null --backend xrender --vsync --no-frame-pacing --no-fading-openclose --no-fading-destroyed-argb --use-ewmh-active-win
+# picom -b --config /dev/null --backend glx --glx-no-stencil --vsync --no-fading-openclose --no-fading-destroyed-argb --use-ewmh-active-win
+# picom -b --config /dev/null --backend glx --glx-no-stencil --glx-no-rebind-pixmap --vsync  --no-frame-pacing --no-fading-openclose --no-fading-destroyed-argb --unredir-if-possible --xrender-sync-fence --use-ewmh-active-win 
 slstatus &
 feh --bg-fill "${HOME}/pictures/groot-dark.png"
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
